88240de2cef081517bec4630399ed211f44d4858
Jordi <jeroig@idonia.com>
2020-11-15 18:42:30 +0100

    add new images

--- vue.config.js
+++ vue.config.js
@@ -11,6 +11,17 @@ module.exports = {
     }
   },
   pwa: {
-    themeColor: '#F7FAFC'
-  }
+    manifestOptions: {
+      display: 'fullscreen'
+    },
+    themeColor: '#F7FAFC',
+    msTileColor: '#F7FAFC',
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/msapplication-icon-144x144.png'
+    }
+  },
 }
